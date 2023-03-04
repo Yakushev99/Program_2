4042ccf5a1876261fa2db966cad8e4822702f901
MYAkushev <m.yakushev.99@gmail.com>
2023-03-04 18:14:49 +0500

    Commit

---
+++ src/ru/ibs/types/Types.java
@@ -0,0 +1,13 @@
+package ru.ibs.types;
+
+public class Types {
+    public static void main(String[] args) {
+
+        double numberDouble = 1234567.123;
+        byte numberByte = 127;
+
+        System.out.println(numberDouble);
+        System.out.println(numberByte);
+
+    }
+}
